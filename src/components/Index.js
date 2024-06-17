e13701470974a8d60a93c7a244d5b0c759d0e883
Bishal-laha <bishallaha12345@gmail.com>
2024-06-18 01:50:40 +0530

    Update Index.js

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,8 +3,8 @@ import AnimatedBoxMobile from "./AnimatedBoxMobile";
 import FileCompress from "./FileCompress";
 import FileConverter from "./FileConverter";
 import FileDragDrop from "./FileDragDrop";
-import FileMerge from "./FileMerge";
-import FileSplit from "./FileSplit";
+// import FileMerge from "./FileMerge";
+// import FileSplit from "./FileSplit";
 import ImageCompress from "./ImageCompress";
 import ImageCrop from "./ImageCrop";
 import ImageDragDrop from "./ImageDragDrop";
@@ -14,4 +14,4 @@ import Cursor from "./Framer/Cursor";
 import Layout from "./Framer/Layout";
 import Redo from "./Framer/Redo";
 
-export {AnimatedBox,AnimatedBoxMobile,FileCompress,FileConverter,FileDragDrop,FileMerge,FileSplit,ImageCompress,ImageCrop,ImageDragDrop,ImageRemoveBg,AnimText,Cursor,Layout,Redo};
+export {AnimatedBox,AnimatedBoxMobile,FileCompress,FileConverter,FileDragDrop,ImageCompress,ImageCrop,ImageDragDrop,ImageRemoveBg,AnimText,Cursor,Layout,Redo};
